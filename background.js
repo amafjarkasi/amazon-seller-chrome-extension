9a96ebdce20380382e31b7f63361300344e83158
Amaf Jarkasi <65797881+amafjarkasi@users.noreply.github.com>
2021-05-21 08:20:18 -0400

    notification added

--- background.js
+++ background.js
@@ -1,11 +1,18 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-        var endpoint_url = 'https://hook.integromat.com/juqo1kw4cp5r77v75l0icdststkdgm80';        
+    var options = {
+        title: 'Status',
+        message: 'Seller information successfully submitted',
+        iconUrl: '/icon.png',
+        type: 'basic' 
+    }
+
+    var endpoint_url = 'https://hook.integromat.com/juqo1kw4cp5r77v75l0icdststkdgm80';        
         title = tab.title;
         url = tab.url;
         console.log(endpoint_url);
-        var xmlhttp = new XMLHttpRequest();
+    var xmlhttp = new XMLHttpRequest();
         xmlhttp.open("POST", endpoint_url);
         xmlhttp.setRequestHeader("Content-Type", "application/json");
-        xmlhttp.send(JSON.stringify({url: url, title: title}));
+        xmlhttp.send(JSON.stringify({url: url, title: title}))
+        chrome.notifications.create('', options);
     });
-
